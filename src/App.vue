a4fb2402f04401ad958732678bd7256172a52d20
skillforce <skillforce@mail.ru>
2022-02-21 14:32:16 +0300

    cryptonomikon part 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
-<template>`Hello`</template>
+<template>
+  <input v-model="ticker" v-on:keydown.enter="add" />
+  <button @click="add">ADD</button>
+  <template v-if="tickers.length">
+    <div style="border: 2px solid grey">
+      <div style="display: flex" v-for="t in tickers" :key="t.id">
+        {{ t.id }} user: {{ t.title }}
+        <button @click="deleteTick(t.id)">X</button>
+      </div>
+    </div>
+  </template>
+</template>
 
 <script>
 export default {
-    name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      ticker: "",
+      tickers: [
+        { title: "Denis", id: 1 },
+        { title: "Olga", id: 2 },
+        { title: "Polina", id: 3 }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      const newTicker = {
+        title: this.ticker,
+        id: this.tickers.length + 1
+      };
+      this.ticker
+        ? this.tickers.push(newTicker)
+        : alert("Firstly enter any name!");
+      this.ticker = null;
+    },
+    deleteTick(id) {
+      this.tickers = this.tickers.filter((t) => t.id !== id);
+    }
+  }
+};
 </script>
 
 <style></style>
