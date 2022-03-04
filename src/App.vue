4e5b19b30d1672cde17f801545f61f44be23a54e
skillforce <skillforce@mail.ru>
2022-03-04 17:54:40 +0300

    cut on components start

--- src/App.vue
+++ src/App.vue
@@ -3,93 +3,34 @@
     <div class="container">
       <div class="w-full my-4"></div>
       <div v-if="loadingStatus === 'loading'">Processing...</div>
-      <section v-if="loadingStatus === 'success'">
-        <div class="flex">
-          <div class="max-w-xs">
-            <label for="wallet" class="block text-sm font-medium text-gray-700">
-              Тикер
-            </label>
-            <div class="mt-1 relative rounded-md shadow-md">
-              <input
-                v-model="ticker"
-                @keydown.enter="add"
-                @keyup="hintsGenerator($event)"
-                type="text"
-                name="wallet"
-                id="wallet"
-                class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                placeholder="Например DOGE"
-                :class="{
-                  'border-red-700 border-2 focus:ring-red-700 focus:border-red-700':
-                    validationError
-                }"
-              />
-            </div>
-            <div class="text-red-700 font-bold" v-if="validationError">
-              {{ validationError }}
-            </div>
-            <template v-if="hintsList.length > 0">
-              <div class="flex">
-                <div
-                  class="border-2 w-1/4 rounded-lg m-1 text-center p-1.5 cursor-pointer bg-gray-700 text-white"
-                  v-for="(hint, i) in hintsList"
-                  :key="i"
-                  @click="hintsUsingHandler(hint)"
-                >
-                  {{ hint }}
-                </div>
-              </div>
-            </template>
-          </div>
-        </div>
-        <button
-          @click="add"
-          type="button"
-          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
-          <svg
-            class="-ml-0.5 mr-2 h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="#ffffff"
-          >
-            <path
-              d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-            ></path>
-          </svg>
-          Add
-        </button>
+      <add-ticker />
+      <div>
         <div>
-          <div>
-            Filter
-            <input
-              v-model="filter"
-              @input="page = 1"
-              class="mt-1 block w-1/4 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-              type="text"
-            />
-          </div>
-          <div>
-            <button
-              @click="pageChangeBtn('decrement')"
-              v-if="page > 1"
-              class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              back
-            </button>
-            <button
-              v-if="hasNextPage"
-              @click="pageChangeBtn('increment')"
-              class="my-4 ml-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              next
-            </button>
-          </div>
+          Filter
+          <input
+            v-model="filter"
+            @input="page = 1"
+            class="mt-1 block w-1/4 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            type="text"
+          />
         </div>
-      </section>
+        <div>
+          <button
+            @click="pageChangeBtn('decrement')"
+            v-if="page > 1"
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            back
+          </button>
+          <button
+            v-if="hasNextPage"
+            @click="pageChangeBtn('increment')"
+            class="my-4 ml-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            next
+          </button>
+        </div>
+      </div>
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
@@ -204,9 +145,13 @@ import {
   unsubscribeFromTicker,
   BAD_TICKERS
 } from "@/api";
+import AddTicker from "@/components/AddTicker.vue";
 
 export default {
   name: "App",
+  components: {
+    AddTicker
+  },
 
   data() {
     return {
---
+++ src/components/AddTicker.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>lalalal</div>
+  <!--  <section>-->
+  <!--    <div class="flex">-->
+  <!--      <div class="max-w-xs">-->
+  <!--        <label for="wallet" class="block text-sm font-medium text-gray-700">-->
+  <!--          Тикер-->
+  <!--        </label>-->
+  <!--        <div class="mt-1 relative rounded-md shadow-md">-->
+  <!--          <input-->
+  <!--            v-model="ticker"-->
+  <!--            @keydown.enter="add"-->
+  <!--            @keyup="hintsGenerator($event)"-->
+  <!--            type="text"-->
+  <!--            name="wallet"-->
+  <!--            id="wallet"-->
+  <!--            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"-->
+  <!--            placeholder="Например DOGE"-->
+  <!--            :class="{-->
+  <!--              'border-red-700 border-2 focus:ring-red-700 focus:border-red-700':-->
+  <!--                validationError-->
+  <!--            }"-->
+  <!--          />-->
+  <!--        </div>-->
+  <!--        <div class="text-red-700 font-bold" v-if="validationError">-->
+  <!--          {{ validationError }}-->
+  <!--        </div>-->
+  <!--        <template v-if="hintsList.length > 0">-->
+  <!--          <div class="flex">-->
+  <!--            <div-->
+  <!--              class="border-2 w-1/4 rounded-lg m-1 text-center p-1.5 cursor-pointer bg-gray-700 text-white"-->
+  <!--              v-for="(hint, i) in hintsList"-->
+  <!--              :key="i"-->
+  <!--              @click="hintsUsingHandler(hint)"-->
+  <!--            >-->
+  <!--              {{ hint }}-->
+  <!--            </div>-->
+  <!--          </div>-->
+  <!--        </template>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <add-button @click="add" />-->
+  <!--  </section>-->
+</template>
+
+<script>
+// import AddButton from "@/components/AddButton.vue";
+
+export default {
+  name: "AddTicker",
+  components: {
+    // AddButton
+  }
+};
+</script>
